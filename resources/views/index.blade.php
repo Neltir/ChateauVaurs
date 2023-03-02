e789515176e3367614cd935a3ce1976fc2fe399f
Neltir <ptitejess19@gmail.com>
2023-03-02 15:55:45 +0100

    Oublie de commit et push la semaine dernière

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/layout')
+@section('content')
+    <div class="parallax">
+        <div class="content">
+            <h2>Château de Vaurs - Gîte à Beaulieu-sur-Dordogne</h2>
+            <p>
+                + Deux appartements à louer à quelques kilomètres des lieux populaires de la région : Lascaux, Rocamadour, Padirac, Collonges-la-Rouge, Château de Turenne, Château de Castelnau.
+            </p>
+            <p>
+                + Un environnement calme, arboré et authentique.
+            </p>
+        </div>
+    </div>
+@endsection
